5c06c9dea3c5c0e89e17a3bc6bf63fdb409ebf8e
Shlok <100297780+CriTMx@users.noreply.github.com>
2026-08-30 03:00:16 +0530

    implement on item pickup event

---
+++ src/main/java/com/critmx/improveditempickups/client/events/ClientEventsHandler.java
@@ -0,0 +1,9 @@
+package com.critmx.improveditempickups.client.events;
+
+import com.critmx.improveditempickups.ImprovedItemPickups;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = ImprovedItemPickups.MODID)
+public class ClientEventsHandler {
+
+}
